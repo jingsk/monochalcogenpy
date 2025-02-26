415f873a849bdc73e126318617f49f585119df63
jingsk <jingsk129@gmail.com>
2025-02-26 15:37:43 -0800

    adjusted convention so that now convention from Salvador Barraza-Lopez's group is followed - a1 (a2) is the so-called arm-chair (zigzag) direction] (see 10.1103/RevModPhys.93.011001).

--- monochalcogenpy/struct.py
+++ monochalcogenpy/struct.py
@@ -3,20 +3,23 @@ from ase import Atoms
 import numpy as np
 
 def unit_cell(a, b, c, use_symm=False):
+    #replacing #b with c
+    #replacing #c with a
+    #replacing #a with b
     cell = np.diag([a,b,c])
     #define Se-Se height 
-    h = np.sqrt(3 - (c/a)**2)/2 * a /b
+    h = np.sqrt(3 - (a/b)**2)/2 * b /c
     #angle relative of Se-Ge vector to vdw direction along the ac plane
-    theta = np.arctan(np.sqrt(2)) - np.arctan(2*h*b/c)
+    theta = np.arctan(np.sqrt(2)) - np.arctan(2*h*c/a)
     #displacement 
-    z_Ge = 1/np.sqrt(2) * a * np.cos(theta) / b
-    y_Ge = 1/np.sqrt(2) * a * np.sin(theta) / c
+    z_Ge = 1/np.sqrt(2) * b * np.cos(theta) / c
+    x_Ge = 1/np.sqrt(2) * b * np.sin(theta) / a
     if not use_symm:
         pos = np.array([
-            [0, 0.5 - h / 2+z_Ge, 0 + y_Ge], #Ge1
-            [0.5, 0.5 + h / 2-z_Ge, 0.5 + y_Ge], #Ge2
-            [0, 0.5 - h / 2, 0],  #Se1
-            [0.5, 0.5 + h / 2, 0.5],  #Se2
+            [0   + x_Ge, 0,   0.5 - h / 2+z_Ge, ], #Ge1
+            [0.5 + x_Ge, 0.5, 0.5 + h / 2-z_Ge], #Ge2
+            [0,          0,   0.5 - h / 2],  #Se1
+            [0.5,        0.5, 0.5 + h / 2],  #Se2
             ])
         atoms = Atoms(
             'Ge2Se2',
@@ -27,8 +30,8 @@ def unit_cell(a, b, c, use_symm=False):
     else:
         from ase.spacegroup import crystal
         basis = np.array([
-            [0, 0.5 - h / 2+z_Ge, 0 + y_Ge], #Ge
-            [0, 0.5 - h / 2, 0]              #Se
+            [0 + x_Ge, 0, 0.5 - h / 2+z_Ge], #Ge
+            [0,        0, 0.5 - h / 2]              #Se
         ]) 
         atoms = crystal(
         ('Ge', 'Se'), 
