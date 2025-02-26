1685017032793c0284e893b233f38a3d472ae62f
jingsk <jingsk129@gmail.com>
2025-02-26 15:28:57 -0800

    copied and pasted from current working code of GeSe cell construction with symmetry. There is a big problem with ase.spacegroup.crystal as it uses ase.spacegroup.Spacegroup. See more here: https://gitlab.com/ase/ase/-/issues/1534

--- monochalcogenpy/struct.py
+++ monochalcogenpy/struct.py
@@ -2,7 +2,7 @@
 from ase import Atoms
 import numpy as np
 
-def unit_cell(a, b, c, method):
+def unit_cell(a, b, c, use_symm=False):
     cell = np.diag([a,b,c])
     #define Se-Se height 
     h = np.sqrt(3 - (c/a)**2)/2 * a /b
@@ -11,7 +11,7 @@ def unit_cell(a, b, c, method):
     #displacement 
     z_Ge = 1/np.sqrt(2) * a * np.cos(theta) / b
     y_Ge = 1/np.sqrt(2) * a * np.sin(theta) / c
-    if method == 'pos':
+    if not use_symm:
         pos = np.array([
             [0, 0.5 - h / 2+z_Ge, 0 + y_Ge], #Ge1
             [0.5, 0.5 + h / 2-z_Ge, 0.5 + y_Ge], #Ge2
@@ -24,4 +24,15 @@ def unit_cell(a, b, c, method):
             cell = cell,
             pbc=[True,True,True]
             )
-        return atoms
+    else:
+        from ase.spacegroup import crystal
+        basis = np.array([
+            [0, 0.5 - h / 2+z_Ge, 0 + y_Ge], #Ge
+            [0, 0.5 - h / 2, 0]              #Se
+        ]) 
+        atoms = crystal(
+        ('Ge', 'Se'), 
+        basis=basis, 
+        spacegroup = 31, 
+        cell=cell)
+    return atoms
