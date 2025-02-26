8a6475b89e916260393cbc6d34fefedf26e0bee5
jingsk <jingsk129@gmail.com>
2025-02-26 15:54:26 -0800

    equivalent_site originally from ase.spacegroup.spacegroup rewritten. Currently using rotation and translation tensors from spglib_dataset rather than from Spacegroup.get_symop(). This avoids problem with Spacegroup mentioned in https://gitlab.com/ase/ase/-/issues/1534

--- monochalcogenpy/struct.py
+++ monochalcogenpy/struct.py
@@ -1,6 +1,81 @@
 #so empty
 from ase import Atoms
 import numpy as np
+import warnings
+
+class SpacegroupError(Exception):
+    """Base exception for the spacegroup module."""
+
+class SpacegroupValueError(SpacegroupError):
+    """Raised when arguments have invalid value."""
+
+
+def equivalent_sites(basis, spg_dataset,symprec = 1e-4):
+    """Create an Atoms instance for a conventional unit cell of a
+    space group. Originally from ase modified by Jing T.
+
+    Parameters:
+
+    basis : list of scaled coordinates
+        Positions of the unique sites corresponding to symbols given
+        either as scaled positions or through an atoms instance.  Not
+        needed if *symbols* is a sequence of Atom objects or an Atoms
+        object.
+        symbols : str | sequence of str | sequence of Atom | Atoms
+        Element symbols of the unique sites.  Can either be a string
+        formula or a sequence of element symbols. E.g. ('Na', 'Cl')
+        and 'NaCl' are equivalent.  Can also be given as a sequence of
+        Atom objects or an Atoms object.
+    spg_dataset : spglib dataset obtained from ase.spacegroup.symmetrize.check_symmetry
+    """
+    #from ase.spacegroup.symmetrize import check_symmetry
+    #s_ds = check_symmetry(atoms)
+    rotations = spg_dataset.rotations
+    translations = spg_dataset.translations
+    
+    def find_orbit(point: np.ndarray) -> np.ndarray:
+        """Find crystallographic orbit of the given point."""
+        candidates = ((rotations @ point) + translations % 1.0) % 1.0
+        orbit = [candidates[0]]
+        for member in candidates[1:]:
+            diff = member - orbit
+            diff -= np.rint(diff)
+            if not np.any(np.all(np.abs(diff) < symprec, axis=1)):
+                orbit.append(member)
+        return np.array(orbit)
+    
+    onduplicates = 'keep'
+    orbits = []
+    for kind, pos in enumerate(basis):
+        for i, (kind0, positions0) in enumerate(orbits):
+            diff = pos - positions0
+            diff -= np.rint(diff)
+            if np.any(np.all(np.abs(diff) < symprec, axis=1)):
+                if onduplicates == 'keep':
+                    pass
+                elif onduplicates == 'replace':
+                    orbits[i] = (kind, positions0)
+                elif onduplicates == 'warn':
+                    warnings.warn(
+                        'scaled_positions %d and %d are equivalent' %
+                        (kind0, kind))
+                elif onduplicates == 'error':
+                    raise SpacegroupValueError(
+                        'scaled_positions %d and %d are equivalent' %
+                        (kind0, kind))
+                break
+        else:
+            orbits.append((kind, find_orbit(pos)))
+    
+    kinds = []
+    sites = []
+    for kind, orbit in orbits:
+        kinds.extend(len(orbit) * [kind])
+        sites.append(orbit)
+    
+    return np.concatenate(sites, axis=0), kinds
+
+
 
 def unit_cell(a, b, c, use_symm=False):
     #replacing #b with c
